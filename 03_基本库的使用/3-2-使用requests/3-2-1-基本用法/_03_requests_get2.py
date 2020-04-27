009a9656ee64f60d0c4f29e91b941b1985fd9233
xuelang201201 <xuelang201201@gmail.com>
2020-04-28 02:29:25 +0800

    GET请求抓取网页

---
+++ 03_基本库的使用/3-2-使用requests/3-2-1-基本用法/_03_requests_get2.py
@@ -0,0 +1,10 @@
+import requests
+import re
+
+headers = {
+    'User-Agent': 'Mozilla/5.0 (X11; Linux x86_64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/81.0.4044.122 Safari/537.36'
+}
+r = requests.get("https://www.zhihu.com/explore", headers=headers)
+pattern = re.compile('explore-feed.*?question_link.*?>(.*?)</a>', re.S)
+titles = re.findall(pattern, r.text)
+print(titles)
