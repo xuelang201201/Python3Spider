c1a66b4ff7df78d780b1590036f399d68726fddd
xuelang201201 <xuelang201201@gmail.com>
2020-04-29 05:54:36 +0800

    发送请求后得到的响应

---
+++ 03_基本库的使用/3-2-使用requests/3-2-1-基本用法/_06_响应.py
@@ -0,0 +1,10 @@
+import requests
+
+r = requests.get('https://www.youku.com')
+print(type(r.status_code), r.status_code)
+print(type(r.headers), r.headers)
+print(type(r.cookies), r.cookies)
+print(type(r.url), r.url)
+print(type(r.history), r.history)
+
+exit() if not r.status_code == requests.codes.ok else print('Request Successfully')
