b4d77b2ac763b9ca8cbec7078e158e3f5a672b07
xuelang201201 <xuelang201201@gmail.com>
2020-04-29 08:00:10 +0800

    Prepared Request 将请求表示为数据结构

---
+++ 03_基本库的使用/3-2-使用requests/3-2-2-高级用法/_08_Prepared_Request.py
@@ -0,0 +1,15 @@
+from requests import Request, Session
+
+url = 'http://httpbin.org/post'
+data = {
+    'name': 'sarah'
+}
+headers = {
+    'User-Agent': 'Mozilla/5.0 (X11; Linux x86_64) AppleWebKit/537.36 (KHTML, like Gecko) '
+                  'Chrome/81.0.4044.122 Safari/537.36'
+}
+s = Session()
+req = Request('POST', url, data=data, headers=headers)
+prepped = s.prepare_request(req)
+r = s.send(prepped)
+print(r.text)
