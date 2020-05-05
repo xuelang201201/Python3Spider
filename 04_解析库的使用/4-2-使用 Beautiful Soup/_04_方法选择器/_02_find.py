3d617efb525125d76c5ffba6de768de54de80062
xuelang201201 <xuelang201201@gmail.com>
2020-05-05 08:36:15 +0800

    find()

---
+++ 04_解析库的使用/4-2-使用 Beautiful Soup/_04_方法选择器/_02_find.py
@@ -0,0 +1,35 @@
+from bs4 import BeautifulSoup
+
+html = '''
+<div class="panel">
+<div class="panel-heading">
+<h4>Hello</h4>
+</div>
+<div class="panel-body">
+<ul class="list" id="list-1">
+<li class="element">Foo</li>
+<li class="element">Bar</li>
+<li class="element">Jay</li>
+</ul>
+<ul class="list list-small" id="list-2">
+<li class="element">Foo</li>
+<li class="element">Bar</li>
+</ul>
+</div>
+</div>
+'''
+soup = BeautifulSoup(html, 'lxml')
+print(soup.find(name='ul'))
+# <ul class="list" id="list-1">
+# <li class="element">Foo</li>
+# <li class="element">Bar</li>
+# <li class="element">Jay</li>
+# </ul>
+print(type(soup.find(name='ul')))
+# <class 'bs4.element.Tag'>
+print(soup.find(class_='list'))
+# <ul class="list" id="list-1">
+# <li class="element">Foo</li>
+# <li class="element">Bar</li>
+# <li class="element">Jay</li>
+# </ul>
