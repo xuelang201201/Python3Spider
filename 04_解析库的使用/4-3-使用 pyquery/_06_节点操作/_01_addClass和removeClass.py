50f17fee86fb93309b06041d2f59106cd15ea11e
xuelang201201 <xuelang201201@gmail.com>
2020-05-05 10:50:07 +0800

    addClass() 和 remove()这些方法可以动态改变节点的 class 属性。

---
+++ 04_解析库的使用/4-3-使用 pyquery/_06_节点操作/_01_addClass和removeClass.py
@@ -0,0 +1,25 @@
+from pyquery import PyQuery as pq
+
+html = '''
+<div class="wrap">
+<div id="container">
+<ul class="list">
+<li class="item-0">first item</li>
+<li class="item-1"><a href="link2.html">second item</a></li>
+<li class="item-0 active"><a href="link3.html"><span class="bold">third item</span></a></li>
+<li class="item-1 active"><a href="link4.html">forth item</a></li>
+<li class="item-0"><a href="link5.html">fifth item</a></li>
+</ul>
+</div>
+</div>
+'''
+doc = pq(html)
+li = doc('.item-0.active')
+print(li)
+# <li class="item-0 active"><a href="link3.html"><span class="bold">third item</span></a></li>
+li.remove_class('active')
+print(li)
+# <li class="item-0"><a href="link3.html"><span class="bold">third item</span></a></li>
+li.add_class('active')
+print(li)
+# <li class="item-0 active"><a href="link3.html"><span class="bold">third item</span></a></li>
