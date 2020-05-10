64882c2260774b814332620c8d5651a7f1f1efe1
xuelang201201 <xuelang201201@gmail.com>
2020-05-10 18:38:37 +0800

    CSV文件存储

--- 05_数据存储/01-文件存储/_03_CSV文件存储.py
+++ 05_数据存储/01-文件存储/_03_CSV文件存储.py
@@ -1,8 +1,44 @@
 import csv
+import pandas as pd
 
+
+"""写入"""
 with open('data.csv', 'w') as csvfile:
-    writer = csv.writer(csvfile)
+    writer = csv.writer(csvfile, delimiter=' ')  # 修改列与列之间的分割符，可以传入 delimiter 参数
     writer.writerow(['id', 'name', 'age'])
     writer.writerow(['10001', 'Mike', '20'])
     writer.writerow(['10002', 'Bob', '22'])
     writer.writerow(['10003', 'Jordan', '21'])
+
+with open('data.csv', 'w') as csvfile:
+    writer = csv.writer(csvfile)
+    writer.writerow(['id', 'name', 'age'])
+    writer.writerows([['10001', 'Mike', '20'], ['10002', 'Bob', '22'], ['10003', 'Jordan', '21']])
+
+with open('data.csv', 'w') as csvfile:
+    fieldnames = ['id', 'name', 'age']
+    writer = csv.DictWriter(csvfile, fieldnames=fieldnames)
+    writer.writeheader()
+    writer.writerow({'id': '10001', 'name': 'Mike', 'age': '20'})
+    writer.writerow({'id': '10002', 'name': 'Bob', 'age': '22'})
+    writer.writerow({'id': '10003', 'name': 'Jordan', 'age': '21'})
+
+with open('data.csv', 'a') as csvfile:  # 追加
+    fieldnames = ['id', 'name', 'age']
+    writer = csv.DictWriter(csvfile, fieldnames=fieldnames)
+    writer.writerow({'id': '10004', 'name': 'Durant', 'age': 22})
+
+with open('data.csv', 'a', encoding='utf-8') as csvfile:
+    fieldnames = ['id', 'name', 'age']
+    writer = csv.DictWriter(csvfile, fieldnames=fieldnames)
+    writer.writerow({'id': '10005', 'name': '王伟', 'age': 22})
+
+"""读取"""
+with open('data.csv', 'r', encoding='utf-8') as csvfile:
+    reader = csv.reader(csvfile)
+    for row in reader:
+        print(row)
+
+# 利用 pandas 读取 CSV 文件
+df = pd.read_csv('data.csv')
+print(df)
