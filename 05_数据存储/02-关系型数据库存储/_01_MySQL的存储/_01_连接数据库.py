e566231bfbfa2293bb839dfadaf9b2b759e13108
xuelang201201 <xuelang201201@gmail.com>
2020-05-10 19:02:36 +0800

    连接数据库

--- 05_数据存储/02-关系型数据库存储/_01_MySQL的存储/_01_连接数据库.py
+++ 05_数据存储/02-关系型数据库存储/_01_MySQL的存储/_01_连接数据库.py
@@ -1,6 +1,6 @@
 import pymysql
 
-db = pymysql.connect(host='localhost', user='debian-sys-maint', password='k2guMUXboBU0V3jk', port=3306)
+db = pymysql.connect(host='localhost', user='root', password='123456', port=3306)
 cursor = db.cursor()
 cursor.execute('SELECT VERSION()')
 data = cursor.fetchone()
