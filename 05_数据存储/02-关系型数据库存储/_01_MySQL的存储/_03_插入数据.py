2553537d3182f1a5df797f7505d94f896453a477
xuelang201201 <xuelang201201@gmail.com>
2020-05-11 20:14:08 +0800

    插入数据

--- 05_数据存储/02-关系型数据库存储/_01_MySQL的存储/_03_插入数据.py
+++ 05_数据存储/02-关系型数据库存储/_01_MySQL的存储/_03_插入数据.py
@@ -2,21 +2,21 @@ import pymysql
 
 data = {
     'id': '20120001',
-    'user': 'Bob',
+    'name': 'Bob',
     'age': 20
 }
-table = 'students'
-keys = ', '.join(['%s'] * len(data))
-values = ', '.join(['%s'] * len(data))
 db = pymysql.connect(host='localhost', user='root', password='123456', port=3306, db='spiders')
 cursor = db.cursor()
+table = 'students'
+keys = ', '.join(data.keys())
+values = ', '.join(['%s'] * len(data))
 sql = 'INSERT INTO {table}({keys}) VALUES ({values})'.format(table=table, keys=keys, values=values)
 try:
     if cursor.execute(sql, tuple(data.values())):
         print('Successful')
         db.commit()
-except:
-    print('Failed')
+except Exception as reason:
+    print('Failed: ' + str(reason))
     db.rollback()
 db.close()
 
