36a92747e2cecedd6353aae9cd14f93c9a534404
xuelang201201 <xuelang201201@gmail.com>
2020-05-11 20:19:33 +0800

    删除数据

---
+++ 05_数据存储/02-关系型数据库存储/_01_MySQL的存储/_05_删除数据.py
@@ -0,0 +1,18 @@
+import pymysql
+
+db = pymysql.connect(host='localhost', user='root', password='123456', port=3306, db='spiders')
+cursor = db.cursor()
+
+table = 'students'
+condition = 'age > 20'
+
+sql = 'DELETE FROM {table} WHERE {condition}'.format(table=table, condition=condition)
+try:
+    cursor.execute(sql)
+    print('Successful')
+    db.commit()
+except Exception as reason:
+    print('Failed: ' + str(reason))
+    db.rollback()
+
+db.close()
