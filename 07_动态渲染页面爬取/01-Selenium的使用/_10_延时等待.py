44bd965a0139e05d96726eb4e845485fc3c94fcb
xuelang201201 <xuelang201201@gmail.com>
2020-05-13 01:41:48 +0800

    延时等待 http://selenium-python.readthedocs.io/api.html#module-selenium.webdriver.support.expected_conditions

---
+++ 07_动态渲染页面爬取/01-Selenium的使用/_10_延时等待.py
@@ -0,0 +1,20 @@
+from selenium import webdriver
+from selenium.webdriver.common.by import By
+from selenium.webdriver.support.ui import WebDriverWait
+from selenium.webdriver.support import expected_conditions as EC
+
+
+"""1.隐式等待"""
+browser = webdriver.Chrome()
+browser.implicitly_wait(10)
+browser.get('https://www.zhihu.com/explore')
+input_ = browser.find_element_by_class_name('AppHeader-TabsLink')
+print(input_)
+
+"""2.显式等待：效果比隐式等待更好"""
+browser = webdriver.Chrome()
+browser.get('https://www.taobao.com/')
+wait = WebDriverWait(browser, 10)
+input_ = wait.until(EC.presence_of_element_located((By.ID, 'q')))
+button = wait.until(EC.element_to_be_clickable((By.CSS_SELECTOR, '.btn-search')))
+print(input_, button)
