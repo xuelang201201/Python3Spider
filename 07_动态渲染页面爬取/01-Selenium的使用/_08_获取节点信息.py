72794ffef10a48f702ada4095d795392554614fe
xuelang201201 <xuelang201201@gmail.com>
2020-05-13 02:07:12 +0800

    获取节点信息的诸多方法

--- 07_动态渲染页面爬取/01-Selenium的使用/_08_获取节点信息.py
+++ 07_动态渲染页面爬取/01-Selenium的使用/_08_获取节点信息.py
@@ -3,26 +3,26 @@ from selenium.webdriver.common.by import By
 
 """1.获取属性"""
 browser = webdriver.Chrome()
-url = 'https://www.zhihu.com/explore'
+url = 'https://book.douban.com/'
 browser.get(url)
-logo = browser.find_element_by_class_name('AppHeader-TabsLink')
-print(logo)
-print(logo.get_attribute('class'))
+nav = browser.find_element_by_id('db-nav-book')
+print(nav)
+print(nav.get_attribute('class'))
 browser.close()
 
 """2.获取文本值"""
 browser = webdriver.Chrome()
-url = 'https://www.zhihu.com/explore'
+url = 'https://book.douban.com/'
 browser.get(url)
-input_ = browser.find_element_by_class_name('AppHeader-login')
-print(input_.text)
+logo = browser.find_element_by_class_name('nav-logo')
+print(logo.text)
 browser.close()
 
 """3.获取 id、位置、标签名和大小"""
 browser = webdriver.Chrome()
-url = 'https://www.zhihu.com/explore'
+url = 'https://book.douban.com/'
 browser.get(url)
-input_ = browser.find_element_by_class_name('AppHeader-TabsLink')
+input_ = browser.find_element_by_class_name('nav-logo')
 print(input_.id)
 print(input_.location)
 print(input_.tag_name)
