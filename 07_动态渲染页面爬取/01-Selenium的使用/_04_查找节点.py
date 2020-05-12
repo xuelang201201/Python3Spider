95082514394a447388fa32f9e4d451df2c7b8aac
xuelang201201 <xuelang201201@gmail.com>
2020-05-12 22:56:21 +0800

    查找节点

--- 07_动态渲染页面爬取/01-Selenium的使用/_04_查找节点.py
+++ 07_动态渲染页面爬取/01-Selenium的使用/_04_查找节点.py
@@ -1,10 +1,35 @@
-"""1.单个节点"""
-from selenium import webdriver
+import time
 
+from selenium import webdriver
+from selenium.webdriver.common.by import By
+
+"""1.单个节点"""
 browser = webdriver.Chrome()
 browser.get('https://www.taobao.com')
 input_first = browser.find_element_by_id('q')
+# input_first = browser.find_element(By.ID, 'q')  # 也可以使用 find_element() 方法
 input_second = browser.find_element_by_css_selector('#q')
 input_third = browser.find_element_by_xpath('//*[@id="q"]')
 print(input_first, input_second, input_third)
 browser.close()
+
+"""2.多个节点"""
+browser = webdriver.Chrome()
+browser.get('https://www.taobao.com')
+lis = browser.find_elements_by_css_selector('.service-bd li')
+# lis = browser.find_elements(By.CSS_SELECTOR, '.service-bd li')  # 也可以使用 find_elements() 方法
+print(lis)
+browser.close()
+
+"""3.节点交互"""
+browser = webdriver.Chrome()
+browser.get('https://www.taobao.com')
+input_ = browser.find_element_by_id('q')
+input.send_keys('iPhone')
+time.sleep(1)
+input_.send_keys('iPhone')
+time.sleep(1)
+input_.clear()
+input_.send_keys('iPad')
+button = browser.find_element_by_class_name('btn-search')
+button.click()
