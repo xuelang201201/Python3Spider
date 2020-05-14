cab212f11bbef532613f5b6bfae57e1534ee0450
xuelang201201 <xuelang201201@gmail.com>
2020-05-14 22:50:10 +0800

    browser = webdriver.Chrome() 不要放在方法里面，否则 Chrome 会闪退。Firefox 没有这样的问题。

--- 07_动态渲染页面爬取/01-Selenium的使用/selenium_demo.py
+++ 07_动态渲染页面爬取/01-Selenium的使用/selenium_demo.py
@@ -85,25 +85,19 @@ def execute_javascript():
 
 def get_element_infos():
     """获取节点信息"""
+    url = 'https://book.douban.com/'
+    browser.get(url)
 
     """1.获取属性"""
-    url = 'https://book.douban.com'
-    browser.get(url)
     nav = browser.find_element_by_id('db-nav-book')
     print(nav)
     print(nav.get_attribute('class'))
-    browser.close()
 
     """2.获取文本值"""
-    url = 'https://book.douban.com'
-    browser.get(url)
     logo = browser.find_element_by_class_name('nav-logo')
     print(logo.text)
-    browser.close()
 
     """3.获取 id、位置、标签名和大小"""
-    url = 'https://book.douban.com'
-    browser.get(url)
     input_ = browser.find_element_by_class_name('nav-logo')
     print(input_.id)
     print(input_.location)
